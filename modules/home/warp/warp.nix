041b38a81c3465c6c53c66988b731455278f9074
djnawara <djnawara@gmail.com>
2025-10-17 11:22:56 -0400

    Improved zshell compatability

--- modules/home/warp/warp.nix
+++ modules/home/warp/warp.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  home.file.".local/share/warp-terminal/themes/daveloper.yml".source = ./daveloper.yml;
+  home.file.".local/share/warp-terminal/themes/".source = pkgs.fetchFromGitHub {
+    owner = "warpdotdev";
+    repo = "themes";
+    rev = "HEAD";
+    sha256 = "8GHUFs1XAIuT+hF41n2zSMePTjaC2PCcVAFApVe6LDA=";
+   };
 }
