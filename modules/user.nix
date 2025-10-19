c676b37d4789a0214e092dca64c85e3c12196e13
Dave Nawara <djnawara@gmail.com>
2025-10-19 18:22:27 -0400

    Removed an unused group from my user

--- modules/user.nix
+++ modules/user.nix
@@ -4,7 +4,7 @@
   users.users.dave = {
     isNormalUser = true;
     description = "Daveloper";
-    extraGroups = [ "networkmanager" "wheel" "docker" "wireshark" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [ pkgs.zsh pkgs.oh-my-zsh pkgs.oh-my-posh ];
     hashedPassword = "$y$j9T$T0jcPnU/EAR1i94/y0JbM1$RF6H4zXKvSeR34OGFU0i9F/3XgEkU8FgKNzROcEqOdC";
     shell = pkgs.zsh;
