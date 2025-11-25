e1bfec4e38e2349d14271729f12e93162d5fa1ce
Dave Nawara <djnawara@gmail.com>
2025-11-24 22:48:46 -0500

    Added helix, zoom, intellij ult and rustrover

--- modules/jetbrains.nix
+++ modules/jetbrains.nix
@@ -3,8 +3,10 @@
 {
   environment.systemPackages = with pkgs; [
     pkgs.jetbrains.clion
+    pkgs.jetbrains.idea-ultimate
     pkgs.jetbrains.pycharm-professional
     pkgs.jetbrains.rider
     pkgs.jetbrains.ruby-mine
+    pkgs.jetbrains.rust-rover
   ];
 }
