dc8b8d2f8b39849dffb217e0d4671e7a260d4738
Dave Nawara <djnawara@gmail.com>
2025-12-01 11:46:01 -0500

    Updated all the things (ran a flake update)

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,11 +4,13 @@
   programs = {
     git = {
       enable = true;
-      user = {
-        name = "Dave Nawara";
-        email = "djnawara@gmail.com";
-      };
+
       settings = {
+        user = {
+          name = "Dave Nawara";
+          email = "djnawara@gmail.com";
+        };
+
         init.defaultBranch = "main";
         pull.rebase = true;
         color.ui = "auto";
